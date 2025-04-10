f9f24b21f17ead75e0a027d835730220b84ff96f
Attila Csermely <csermely.a.dev@gmail.com>
2025-04-10 14:19:42 +0200

    setup vote endpoint

--- src/routes/vote/+server.ts
+++ src/routes/vote/+server.ts
@@ -1,7 +1,16 @@
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 
 export async function POST({ request }) {
 	const { id } = await request.json();
+
+	const response = await fetch(`http://localhost:3000/vote/${id}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+
+	if (response.status != 200) error(response.status)
 
 	return json({ id }, { status: 200 });
 }
